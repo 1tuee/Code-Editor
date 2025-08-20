5a4f0c02e97b658aed9443abd0e52ff09197354f
1tue <cxsperb@gmail.com>
2025-08-19 19:36:24 -0700

    rust!

---
+++ target/debug/deps/main.d
@@ -0,0 +1,5 @@
+C:\Users\Charles\OneDrive\Documentos\GitHub\Code-Editor\target\debug\deps\main.d: src\main.rs
+
+C:\Users\Charles\OneDrive\Documentos\GitHub\Code-Editor\target\debug\deps\main.exe: src\main.rs
+
+src\main.rs:
