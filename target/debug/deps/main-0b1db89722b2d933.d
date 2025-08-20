a239b11790c0aea8d2ae91626b19a4e1e7cba2d8
1tue <cxsperb@gmail.com>
2025-08-19 19:44:54 -0700

    brhu

---
+++ target/debug/deps/main-0b1db89722b2d933.d
@@ -0,0 +1,5 @@
+C:\Users\Charles\OneDrive\Documentos\GitHub\Code-Editor\target\debug\deps\main-0b1db89722b2d933.d: src\main.rs
+
+C:\Users\Charles\OneDrive\Documentos\GitHub\Code-Editor\target\debug\deps\libmain-0b1db89722b2d933.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/main-bbd2ec5a9dc6e279.d
@@ -0,0 +1,5 @@
+C:\Users\Charles\OneDrive\Documentos\GitHub\Code-Editor\target\debug\deps\main-bbd2ec5a9dc6e279.d: src\main.rs
+
+C:\Users\Charles\OneDrive\Documentos\GitHub\Code-Editor\target\debug\deps\libmain-bbd2ec5a9dc6e279.rmeta: src\main.rs
+
+src\main.rs:
